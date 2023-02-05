a16ed5893208f3f9f04cb1ad5afa29e292e1576f
youssef khemakhem <youssef.khemakhem@esprit.tn>
2023-02-06 00:07:40 +0100

    first commit

---
+++ src/Controller/TeacherController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class TeacherController extends AbstractController
+{
+    #[Route('/teacher/{name}', name: 'app_teacher')]
+   
+    public function showTeacher($name)
+    {
+        return $this->render('teacher/showTeacher.html.twig', [
+            'name' => $name,
+        ]);
+    }
+
+    #[Route('/redirectostudent', name: 'redirect_to_student')]
+    public function RedirectToStudent()
+    {
+        return $this->redirectToRoute('app_student');
+    }
+}
